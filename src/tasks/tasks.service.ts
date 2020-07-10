d3c1cd6ecf33ce5b5257b150fa9c5d9d9faef91c
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2020-07-10 17:47:31 +0100

    Feat: get all tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Task {}
+
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks() {
+    return this.tasks;
+  }
+}
