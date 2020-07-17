ffb2140d4937a0a408574b7f9c8ef1b0736444a1
Daute Rodriguez Rodriguez <dauterguezrguez@gmail.com>
2020-07-17 23:15:44 +0100

    Feat: signup validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,8 +1,24 @@
-import { IsNotEmpty } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  MaxLength,
+  Matches
+} from 'class-validator';
 
 export class AuthCredentialsDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
 
   @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password too weak'
+  })
   password: string;
 }
