5dbe07acea9b55f37dc7a2fab9e0c06d16491bb8
Daute Rodriguez Rodriguez <dauterguezrguez@gmail.com>
2020-07-26 12:31:25 +0100

    Feat: json web tokens

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,21 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    JwtModule.register({
+      // TODO: Set secret as environment variable
+      secret: 'supermegasecret',
+      signOptions: {
+        expiresIn: 3600
+      }
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    TypeOrmModule.forFeature([UserRepository])
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
