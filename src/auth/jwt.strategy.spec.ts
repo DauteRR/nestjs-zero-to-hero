0453bdbc160d9ee52aa11437ff9c095f6bbba95c
Daute Rodriguez Rodriguez <dauterguezrguez@gmail.com>
2020-08-09 18:21:09 +0100

    Add: JwtStrategy tests

---
+++ src/auth/jwt.strategy.spec.ts
@@ -0,0 +1,47 @@
+import { JwtStrategy } from './jwt.strategy';
+import { Test } from '@nestjs/testing';
+import { UserRepository } from './user.repository';
+import { User } from './user.entity';
+import { UnauthorizedException } from '@nestjs/common';
+
+const mockUserRepository = () => ({
+  findOne: jest.fn()
+});
+
+describe('JwtStrategy', () => {
+  let jwtStrategy: JwtStrategy;
+  let userRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        JwtStrategy,
+        { provide: UserRepository, useFactory: mockUserRepository }
+      ]
+    }).compile();
+
+    jwtStrategy = await module.get<JwtStrategy>(JwtStrategy);
+    userRepository = await module.get<UserRepository>(UserRepository);
+  });
+
+  describe('validate', () => {
+    it('validates and returns the user based on JWT payload', async () => {
+      const user = new User();
+      user.username = 'test-user';
+      userRepository.findOne.mockResolvedValue(user);
+      const result = await jwtStrategy.validate({ username: 'test-user' });
+
+      expect(userRepository.findOne).toHaveBeenCalledWith({
+        username: 'test-user'
+      });
+      expect(result).toEqual(user);
+    });
+
+    it('throws an unauthorized exception as user cannot be found', () => {
+      userRepository.findOne.mockResolvedValue(null);
+      expect(jwtStrategy.validate({ username: 'test-user' })).rejects.toThrow(
+        UnauthorizedException
+      );
+    });
+  });
+});
